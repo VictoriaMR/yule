5fbe8b8a3f684b507c9211b2bf490f839f8df5d1
LiaoMingRong <849376723@qq.com>
2020-10-27 18:32:12 +0800

    debug

--- app/Http/Controllers/Home/LoginController.php
+++ app/Http/Controllers/Home/LoginController.php
@@ -8,10 +8,15 @@ class LoginController extends Controller
 {
 	public function index()
 	{
-		// $ffcService = make('App/Services/FfcService');
-		// $ffcService->getOriginFfc();
-		// die();
-		
+		$code = iget('code');
+		if (empty($code)) {
+			$secretService = make('App/Services/SecretService');
+			$info = $secretService->getOne();
+			$url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$info['appid'].'&redirect_uri='.url('login').'&response_type=code&scope=SCOPE&state=STATE#wechat_redirec';
+			redirect($url);
+			// https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirec
+		}
+		dd(iget());
 		Html::addCss();
 		Html::addJs();
 
