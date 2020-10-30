c952bb26ea627a6609557b2349f740af9ae6dd1e
LiaoMingRong <849376723@qq.com>
2020-10-30 18:46:42 +0800

    获取授权

--- view/mobile/Home/Login/index.php
+++ view/mobile/Home/Login/index.php
@@ -4,11 +4,6 @@
 	<title>测试授权</title>
 </head>
 <body>
-	<form id="test_form" method="get" action="<?php echo $url;?>"></form>
-	<script type="text/javascript">
-		var form = document.getElementById('test_form');
-		//再次修改input内容
-		form.submit();
-	</script>
+	<a href="<?php echo $url;?>">点击跳转</a>
 </body>
 </html>
