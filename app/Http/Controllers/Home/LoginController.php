d05c7d462a4b1fd2cffbc03deba04a4b5a9eb139
LiaoMingRong <849376723@qq.com>
2020-11-02 17:09:40 +0800

    debug

--- app/Http/Controllers/Home/LoginController.php
+++ app/Http/Controllers/Home/LoginController.php
@@ -2,20 +2,29 @@
 
 namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
+use App\Services\SecretService;
+use frame\Http;
 use frame\Html;
 
 class LoginController extends Controller
 {
+	private $config = [];
+
+	public function __construct(SecretService $service)
+	{
+		$this->config = $service->getOne();
+	}
+
 	public function index()
 	{
-		$code = iget('code');
-		if (empty($code)) {
-			$secretService = make('App/Services/SecretService');
-			$info = $secretService->getOne();
-			$url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$info['appid'].'&redirect_uri='.urlencode(url('login]')).'&response_type=code&scope=snsapi_base&state=bjl#wechat_redirec';
-			$this->assign('url', $url);
-		} else {
-			$info = $this->getInfoByCode($code);
+		if (!empty($this->config)) {
+			$code = iget('code');
+			if (empty($code)) {
+				$url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$this->config['appid'].'&redirect_uri='.urlencode(url('login]')).'&response_type=code&scope=snsapi_base&state=bjl#wechat_redirec';
+				$this->assign('url', $url);
+			} else {
+				$info = $this->getInfoByCode($code);
+			}
 		}
 
 
@@ -24,6 +33,21 @@ class LoginController extends Controller
 
 	protected function getInfoByCode($code)
 	{
-		dd($code);
+		if (empty($code)) {
+			return false;
+		}
+		if (!empty($this->config)) {
+			return false;
+		}
+		$params = [
+			'appid' => $this->config['appid'],
+			'secret' => $this->config['secret'],
+			'code' => $code,
+			'grant_type' => 'authorization_code',
+		];
+		$token = Http::get('https://api.weixin.qq.com/sns/oauth2/access_token', $params);
+		dd($token);
+		//获取access_token
+		https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=${secret}&code=${code}&grant_type=authorization_code
 	}
 }
